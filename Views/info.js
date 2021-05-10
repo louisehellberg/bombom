184ace400f6528288fd1af5e504595f22846698b
Louise <louise-hellberg@hotmail.com>
2021-05-10 14:28:59 +0200

    startscreen

---
+++ Views/info.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import {
+    SafeAreaView,
+    ScrollView,
+    StatusBar,
+    StyleSheet,
+    Text,
+    useColorScheme,
+    View,
+    Button,
+    Alert
+  } from 'react-native';
+
+const InfoScreen = ({navigation}) => {
+    return (
+        <View >
+          <Text>Info</Text>
+        </View>
+      );
+}
+
+export default InfoScreen;
---
+++ Views/start.js
@@ -0,0 +1,121 @@
+import React from 'react';
+import {
+    SafeAreaView,
+    ScrollView,
+    StatusBar,
+    StyleSheet,
+    Text,
+    useColorScheme,
+    View,
+    Button,
+    Alert,
+    Image
+  } from 'react-native';
+
+  import {
+    Colors,
+    DebugInstructions,
+    Header,
+    LearnMoreLinks,
+    ReloadInstructions,
+  } from 'react-native/Libraries/NewAppScreen';
+  
+const Start = ({navigation}) => {
+    const isDarkMode = useColorScheme() === 'dark';
+
+  const backgroundStyle = {
+    backgroundColor: Colors.darker 
+  };
+
+  return (
+    <SafeAreaView style={backgroundStyle}>
+      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
+      <ScrollView
+        contentInsetAdjustmentBehavior="automatic"
+        style={backgroundStyle}>
+        <View style={styles.header}>
+        <Image
+        style={styles.logo}
+        source={require('../Static/bombomlogga_Rityta1.png')}
+      />
+      </View>
+        <View
+          style={{
+            backgroundColor: '#2e2e2e',
+          }}>
+          <Section >
+            Welcome to the classic game: <Text /* style={styles.highlight} */>Choose the right chest</Text> 
+          </Section>
+          
+          
+          {/* <Section title="Learn More">
+            Read the docs to discover what to do next:
+          </Section> */}
+          <Button title="start" onPress={() => Alert.alert("hej")} color="#FFBE5E" style={styles.btn}/>
+          <Button title="How to play" onPress={() => navigation.navigate('Info')} style={styles.btn}/>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+}
+const Section = ({children, title}) => {
+    const isDarkMode = useColorScheme() === 'dark';
+    return (
+      <View style={styles.sectionContainer}> 
+        <Text
+          style={[
+            styles.sectionTitle,
+            {
+              color: isDarkMode ? Colors.white : Colors.black,
+            },
+          ]}>
+          {title}
+        </Text>
+        <Text
+          style={[
+            styles.sectionDescription,
+            {
+              color: isDarkMode ? Colors.light : Colors.dark,
+            },
+          ]}>
+          {children}
+        </Text>
+      </View>
+    );
+  };
+
+  const styles = StyleSheet.create({
+    sectionContainer: {
+      marginTop: 32,
+      paddingHorizontal: 24,
+    },
+    sectionTitle: {
+      fontSize: 24,
+      fontWeight: '600',
+    },
+    sectionDescription: {
+      marginTop: 8,
+      fontSize: 18,
+      fontWeight: '400',
+    },
+    highlight: {
+      fontWeight: '700',
+    },
+  btn: {
+    backgroundColor: "#FFBE5E"
+  },
+  logo: {
+    width: 250,
+    height: 250,
+    backgroundColor: 'white',
+   
+  },
+  header: {
+      justifyContent: 'center',
+      alignItems: 'center',
+      backgroundColor: '#2e2e2e'
+  }
+  
+  });
+
+  export default Start;
