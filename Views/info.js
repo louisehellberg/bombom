006c3a8bead1568bba34e07e6bece89897a90b97
vilmaviola <vilma.ahlholm@live.se>
2021-05-10 14:52:38 +0200

    Button in Info

--- Views/info.js
+++ Views/info.js
@@ -1,7 +1,13 @@
 import React from 'react';
-import { SafeAreaView, Text, View, Button } from 'react-native';
+import { SafeAreaView, Text, View, Button, TouchableOpacity } from 'react-native';
 
 const Info = () => {
+
+  const StartButton = ({ onPress, title }) => (
+    <TouchableOpacity onPress={onPress} style={styles.appButtonContainer}>
+      <Text style={styles.appButtonText}>{title}</Text>
+    </TouchableOpacity>
+  );
 
     return (
       <SafeAreaView>
@@ -15,11 +21,29 @@ const Info = () => {
             </Text>
             <Text style={{fontSize: 15, textAlign: "center"}}>Do you think you can do it?</Text>
            
-            <Button title="START" onPress={() => Alert.alert("Starting game")} color="#FFBE5E" ></Button>
+            <View style={styles.screenContainer}>
+              <StartButton title="Start" size="sm"/>
+            </View>
 
       </SafeAreaView>
     );
   };
+
+  const styles = StyleSheet.create({
+    appButtonContainer: {
+      elevation: 8,
+      backgroundColor: "#FFBE5E",
+      borderRadius: 10,
+      paddingVertical: 2,
+      paddingHorizontal: 2
+    },
+    appButtonText: {
+      fontSize: 18,
+      color: "#2E2E2E",
+      alignSelf: "center",
+      textTransform: "uppercase"
+    }
+  });
 
   export default Info;
   
