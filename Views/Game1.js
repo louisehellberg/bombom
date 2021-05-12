8b514de0dfe16a4a83e19448f1c1e79da593578b
vilmaviola <vilma.ahlholm@live.se>
2021-05-12 10:56:34 +0200

    game2, randomizing which chest gets what pattern

--- Views/Game1.js
+++ Views/Game1.js
@@ -1,62 +1,53 @@
 import React from 'react';
-import {
-    SafeAreaView,
-    StyleSheet,
-    Text,
-    View,
-    TouchableOpacity,
-    Image,
-    Vibration,
-    Button,
-  } from 'react-native';
+import { SafeAreaView, StyleSheet, Text, View, TouchableOpacity, Image, Vibration } from 'react-native';
 import StartButton from '../Components/StartBtn';
-
-
   
 const Game1 = ({navigation}) => {
   const StartButton = ({ title, onPress }) => (
     <TouchableOpacity onPress={onPress} style={styles.appButtonContainer}>
       <Text style={styles.appButtonText}>{title}</Text>
-    </TouchableOpacity>);
+    </TouchableOpacity>
+  );
 
-const ONE_SECOND_IN_MS = 1000;
-
-const PATTERN = [
-  1 * ONE_SECOND_IN_MS,
-  2 * ONE_SECOND_IN_MS,
-  3 * ONE_SECOND_IN_MS
-];
+const vibration_pattern_empty = [1000, 2000, 3000];
+const vibration_pattern_bomb1 = [3000, 1000, 2000];
+const vibration_pattern_bomb2 = [2000, 3000, 1000];
 
 /* ska vi ha massa patterns där de 3 som används slumpas fram(alternativt att bomben r samma och de andra slumpas) eller ska vi bara ha tre där bomben alltid är samma. Därefter ska de randomiseras i game2 */  
-    return (
-        <SafeAreaView >
-         
-        <View style={styles.background}>
-        <Text style={styles.infoTitle}>ARE YOU READY?</Text>
-        </View>
-        <View style={styles.imageContainer}>
-          <TouchableOpacity onPress={() => Vibration.vibrate(PATTERN)}>
-            <Image
-                style={styles.image}
-                source={require('../Assets/open.png')}
-            /></TouchableOpacity>
-            <TouchableOpacity onPress={() => Vibration.vibrate(PATTERN)}>
-            <Image
-                style={styles.image}
-                source={require('../Assets/open.png')}
-            /></TouchableOpacity>
-            <TouchableOpacity onPress={() => Vibration.vibrate(PATTERN)}>
-            <Image
-                style={styles.image}
-                source={require('../Assets/open.png')}
-            /></TouchableOpacity>
-        </View>
-        <View style={styles.buttonContainer}>
-        <StartButton title="Start" size="sm" onPress={() => navigation.navigate('Game2')}/>
-        </View>
+  return (
+      <SafeAreaView >
       
-        </SafeAreaView>
-      );
+        <View style={styles.background}>
+          <Text style={styles.infoTitle}>ARE YOU READY?</Text>
+        </View>
+
+        <View style={styles.imageContainer}>
+          <TouchableOpacity onPress={() => Vibration.vibrate(vibration_pattern_empty)}>
+            <Image
+                style={styles.image}
+                source={require('../Assets/open.png')}
+            />
+          </TouchableOpacity>
+          <TouchableOpacity onPress={() => Vibration.vibrate(vibration_pattern_bomb1)}>
+            <Image
+                style={styles.image}
+                source={require('../Assets/open.png')}
+            />
+          </TouchableOpacity>
+          <TouchableOpacity onPress={() => Vibration.vibrate(vibration_pattern_bomb2)}>
+            <Image
+                style={styles.image}
+                source={require('../Assets/open.png')}
+            />
+          </TouchableOpacity>
+        </View>
+
+        <View style={styles.buttonContainer}>
+          <StartButton title="Start" size="sm" onPress={() => navigation.navigate('Game2')}/>
+        </View>
+    
+      </SafeAreaView>
+  );
 }
 
 const styles = StyleSheet.create({
--- Views/Game2.js
+++ Views/Game2.js
@@ -1,43 +1,48 @@
 import React from 'react';
 import {SafeAreaView, StyleSheet, Text, View, TouchableOpacity, Image, Vibration} from 'react-native';
-import StartButton from '../Components/StartBtn';
   
 const Game2 = ({ navigation }) => {
-  const StartButton = ({ title }) => (
-    <TouchableOpacity onPress={() => navigation.navigate('Confirmation')} style={styles.appButtonContainer}>
-      <Text style={styles.appButtonText}>{title}</Text>
-    </TouchableOpacity>);
-
+  
 const PickChestButton = ({ onPress, title }) => (
   <TouchableOpacity onPress={onPress} style={styles.appButtonContainer}>
     <Text style={styles.appButtonText}>{title}</Text>
   </TouchableOpacity>
 );
 
-const ONE_SECOND_IN_MS = 1000;
+const vibration_pattern_empty = [1000, 2000, 3000];
+const vibration_pattern_bomb1 = [3000, 1000, 2000];
+const vibration_pattern_bomb2 = [2000, 3000, 1000];
 
-const PATTERN_BOMB = [
-  3 * ONE_SECOND_IN_MS,
-  2 * ONE_SECOND_IN_MS,
-  1 * ONE_SECOND_IN_MS
-];
+const outer_vibration_list = [vibration_pattern_empty, vibration_pattern_bomb1, vibration_pattern_bomb2];
+var currentIndex = outer_vibration_list.length, temporaryValue, randomIndex;
 
-const PATTERN1 = [
-  1 * ONE_SECOND_IN_MS,
-  2 * ONE_SECOND_IN_MS,
-  3 * ONE_SECOND_IN_MS
-];
+while (0 !== currentIndex) {
+  // Pick a remaining element...
+  randomIndex = Math.floor(Math.random() * currentIndex);
+  currentIndex -= 1;
 
-const PATTERN2 = [
-  2 * ONE_SECOND_IN_MS,
-  1 * ONE_SECOND_IN_MS,
-  3 * ONE_SECOND_IN_MS
-];
+  // And swap it with the current element.
+  temporaryValue = outer_vibration_list[currentIndex];
+  outer_vibration_list[currentIndex] = outer_vibration_list[randomIndex];
+  outer_vibration_list[randomIndex] = temporaryValue;
+}
 
-var chest_contains_bomb = '';
+const chest_vibration1 = outer_vibration_list[0];
+const chest_vibration2 = outer_vibration_list[1];
+const chest_vibration3 = outer_vibration_list[2];
 
-function randomizer() {
-  
+const chest_button1 = 'true';
+const chest_button2 = 'true';
+const chest_button3 = 'true';
+
+if (chest_vibration1 == vibration_pattern_empty) {
+  chest_button1 = 'false';
+}
+else if (chest_vibration2 == vibration_pattern_empty) {
+  chest_button2 = 'false';
+}
+else if (chest_vibration3 == vibration_pattern_empty) {
+  chest_button3 = 'false';
 }
 
 /* ska vi ha massa patterns där de 3 som används slumpas fram(alternativt att bomben r samma och de andra slumpas) eller ska vi bara ha tre där bomben alltid är samma. Därefter ska de randomiseras i game2 */  
@@ -49,19 +54,19 @@ return (
     </View>
 
     <View style={styles.imageContainer}>
-      <TouchableOpacity onPress={() => Vibration.vibrate(PATTERN_BOMB)}>
+      <TouchableOpacity onPress={() => Vibration.vibrate(chest_vibration1)}>
         <Image
             style={styles.image}
             source={require('../Assets/closed.png')}
         />
       </TouchableOpacity>
-      <TouchableOpacity onPress={() => Vibration.vibrate(PATTERN1)}>
+      <TouchableOpacity onPress={() => Vibration.vibrate(chest_vibration2)}>
         <Image
             style={styles.image}
             source={require('../Assets/closed.png')}
         />
       </TouchableOpacity>
-      <TouchableOpacity onPress={() => Vibration.vibrate(PATTERN2)}>
+      <TouchableOpacity onPress={() => Vibration.vibrate(chest_vibration3)}>
         <Image
             style={styles.image}
             source={require('../Assets/closed.png')}
@@ -72,17 +77,17 @@ return (
     <View style={styles.buttonContainer}>
       <PickChestButton title="Pick" onPress={() => 
         navigation.navigate('Confirmation', {
-          resultParam: 'true',
+          resultParam: chest_button1,
         })}
       />
       <PickChestButton title="Pick" onPress={() => 
         navigation.navigate('Confirmation', {
-          resultParam: 'true',
+          resultParam: chest_button2,
         })}
       />
       <PickChestButton title="Pick" onPress={() => 
         navigation.navigate('Confirmation', {
-          resultParam: 'true',
+          resultParam: chest_button3,
         })}
       />
     </View>
